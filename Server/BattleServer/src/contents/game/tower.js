d11da5bd1adad05a383a8a7bb2961f099214fee8
yth0417 <yth0417@gmail.com>
2024-12-03 11:37:36 +0900

    update: protoc 수정 및 타워 공격 처리
    protoc 수정 및 타워 공격 처리

--- Server/BattleServer/src/contents/game/tower.js
+++ Server/BattleServer/src/contents/game/tower.js
@@ -1,5 +1,9 @@
+import { ePacketId } from 'ServerCore/src/network/packetId.js';
 import { assetManager } from '../../utils/assetManager.js';
 import { GameObject } from './gameObject.js';
+import { PacketUtils } from 'ServerCore/src/utils/packetUtils.js';
+import { B2C_TowerAttackMonsterNotificationSchema } from '../../protocol/tower_pb.js';
+import { B2C_MonsterDeathNotificationSchema } from '../../protocol/monster_pb.js';
 
 export class Tower extends GameObject {
   /*---------------------------------------------
@@ -12,7 +16,7 @@ export class Tower extends GameObject {
    * @type {number} hp
    * @type {number} maxHp
    */
-  attackDamage = 0; //추후에 projectile 클래스를 추가할 예정, 타워는 공격력이 없음
+  attackDamage = 0;
   attackRange = 0;
   attackCoolDown = 0;
   hp = 0;
@@ -39,7 +43,6 @@ export class Tower extends GameObject {
     this.attackCoolDown = towerData?.attackCoolDown;
     this.hp = this.maxHp = towerData?.maxHp;
     this.lastAttackTime = 0; // 마지막 공격 시간
-    this.bullet = [];
   }
 
   /**
@@ -65,34 +68,61 @@ export class Tower extends GameObject {
   }
 
   /**
-   * 타워의 공격 처리
-   * @param {Array} monsters - 몬스터 배열
+   * 타워 공격
+   * @param {object} tower - 타워 객체 { id, x, y }
    */
+
   attackTarget(monsters) {
     const currentTime = Date.now();
-    if (currentTime - this.lastAttackTime < this.attackCoolDown) return;
+    if (currentTime - this.lastAttackTime > this.attackCoolDown) {
+      this.lastAttackTime = currentTime;
 
-    const target = this.getMonsterInRange(monsters);
-    if (target) {
-      // 투사체 생성
-      const bullet = new Bullet(this.pos, target, 0.2, this.attackDamage);
-      this.bullet.push(bullet);
-      console.log('투사체 발사:', bullet);
+      const target = this.getMonsterInRange(monsters);
+
+      // 몬스터 데미지 처리
+      const isDestroyed = target.onDamaged(this.attackDamage);
+
+      // 2. 클라이언트에 공격 패킷 전송
+      const attackPacket = create(B2C_TowerAttackMonsterNotificationSchema, {
+        towerId: this.getId(),
+        posInfo: target.getpos(),
+        hp: target.hp,
+        maxHp: target.maxHp,
+      });
+
+      const attackBuffer = PacketUtils.SerializePacket(
+        attackPacket,
+        B2C_TowerAttackMonsterNotificationSchema,
+        ePacketId.B2C_TowerAttackMonsterNotification,
+        0,
+      );
+      this.room.broadcast(attackBuffer);
+
+      // 3. 타워 파괴 처리
+      if (isDestroyed) {
+        console.log(`타워 ${target.id}가 파괴되었습니다.`);
+        this.room.removeObject(target.id); // GameRoom에서 타워 제거
+
+        const mopnsterDeathPacket = create(B2C_MonsterDeathNotificationSchema, {
+          monsterId: target.id,
+        });
+
+        const monsterDeathBuffer = PacketUtils.SerializePacket(
+          mopnsterDeathPacket,
+          B2C_MonsterDeathNotificationSchema,
+          ePacketId.B2C_MonsterDeathNotification,
+          0, //수정 부분
+        );
+
+        this.room.broadcast(monsterDeathBuffer);
+      }
     }
-
-    this.lastAttackTime = currentTime;
   }
 
   /**
-   * 투사체 이동 처리
+   *
+   * @param {number} attackDamage
    */
-  updateBullet() {
-    this.bullet = this.bullet.filter((bullet) => {
-      bullet.move();
-      return bullet.isActive;
-    });
-  }
-
   onDamaged(attackDamage) {
     this.hp = Math.max(this.hp - attackDamage, 0);
     if (this.hp > 0) this.onDeath();
