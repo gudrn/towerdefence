ad49eb4de0240d79f67016b6d7ea09ab53da5b04
gudrn4616 <gudrn8293@naver.com>
2024-12-17 02:16:15 +0900

    긴급

--- Server/BattleServer/src/config/config.ts
+++ Server/BattleServer/src/config/config.ts
@@ -1,10 +1,10 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const PORT = 3005
+export const PORT = 3005;
 //export const HOST ='ec2-13-125-207-67.ap-northeast-2.compute.amazonaws.com';
-export const HOST ="127.0.0.1";
+export const HOST = '0.0.0.0';
 export const CLIENT_VERSION = process.env.CLIENT_VERSION || '1.0.0';
 
 export const battleConfig = {
--- Server/LobbyServer/src/config/config.ts
+++ Server/LobbyServer/src/config/config.ts
@@ -4,10 +4,10 @@ dotenv.config();
 
 export const PORT = 3000;
 //export const HOST = 'ec2-13-125-207-67.ap-northeast-2.compute.amazonaws.com';
-export const HOST: string = '127.0.0.1';
+export const HOST: string = '0.0.0.0';
 
 export const BATTLE_PORT = 3005;
-export const BATTLE_HOST: string = '127.0.0.1';
+export const BATTLE_HOST: string = '0.0.0.0';
 
 export const CLIENT_VERSION = process.env.CLIENT_VERSION || '1.0.0';
 
