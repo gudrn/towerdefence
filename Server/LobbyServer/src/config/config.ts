6810c0692ae73654a3b40dfd9dfc1dbc59ef3265
gudrn4616 <gudrn8293@naver.com>
2024-12-17 15:11:30 +0900

    수정
    수정

--- Server/LobbyServer/src/config/config.ts
+++ Server/LobbyServer/src/config/config.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const PORT = 3000;
-export const HOST = 'ec2-13-125-199-218.ap-northeast-2.compute.amazonaws.com';
+export const HOST = '0.0.0.0';
 
 export const BATTLE_PORT = 3005;
 export const BATTLE_HOST = 'ec2-13-125-199-218.ap-northeast-2.compute.amazonaws.com';
