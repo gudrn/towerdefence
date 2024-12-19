84c215e4107931050839c08ab54cd087068e8a66
gudrn4616 <gudrn8293@naver.com>
2024-12-19 10:41:19 +0900

    주소 수정

--- Server/LobbyServer/src/utils/redis/redis.ts
+++ Server/LobbyServer/src/utils/redis/redis.ts
@@ -1,5 +1,6 @@
 import exp from 'constants';
 import Redis from 'ioredis';
 
-export const redis = new Redis();
-
+export const redis = new Redis(
+  'redis://ec2-13-125-207-67.ap-northeast-2.compute.amazonaws.com:6379',
+);
