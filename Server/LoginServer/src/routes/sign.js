83132532272c90cd07913f429794e99c16f82716
gudrn4616 <gudrn8293@naver.com>
2024-12-19 16:42:40 +0900

    fix: 로그인 수정

--- Server/LoginServer/src/routes/sign.js
+++ Server/LoginServer/src/routes/sign.js
@@ -5,17 +5,14 @@ import { prisma } from '../utils/prisma/prisma.js';
 import { v4 as uuidv4 } from 'uuid';
 import jwt from 'jsonwebtoken';
 import { signUpSchema, signInSchema } from '../utils/schema/signSchema.js';
-
 const router = express.Router();
-
 /*---------------------------------------------
-            [회원 가입]
-            register->signup으로 변경
+      [회원 가입]
 ---------------------------------------------*/
 router.post('/signup', async (req, res) => {
+  console.log("회원가입");
   const { email, password, nickname } = req.body;
-
-  // 이미 존재하는 ID와 닉네임인지 확인
+  // 이미 존재하는 ID인지 확인
   try {
     await signUpSchema.validate(req.body);
     const hashedPwd = await bcrypt.hash(password, 10);
@@ -23,22 +20,10 @@ router.post('/signup', async (req, res) => {
       where: { email },
       select: { email: true },
     });
-
-    const isExistedNickname = await prisma.users.findUnique({
-      where: { nickname },
-      select: { nickname: true },
-    });
-
     if (isExistedEmail) {
-      console.log('이미 사용 중인 ID입니다.');
-      return res.status(409).json({ message: '이미 사용 중인 ID입니다.' });
+      console.log("이미 사용 중인 id");
+      return res.status(409).json({ message: '이미 사용 중인 이메일입니다.' });
     }
-
-    if (isExistedNickname) {
-      console.log('이미 사용 중인 닉네임입니다.');
-      return res.status(409).json({ message: '이미 사용 중인 닉네임입니다.' });
-    }
-
     await prisma.users.create({
       data: {
         user_id: uuidv4(),
@@ -47,25 +32,23 @@ router.post('/signup', async (req, res) => {
         nickname,
       },
     });
-
+    console.log("성공")
     return res.status(201).json({ email, nickname });
   } catch (err) {
     if (err.name === 'ValidationError') {
-      return res.status(400).json({ message: '유효성 검사 오류', error: err.errors });
+      console.log("유효성 검사 오류");
+      return res.status(400).json({ message: '이메일, 비밀번호를 요구조건에 맞게 입력해주세요' });
     }
-    console.error(err);
     return res.status(500).json({ message: '계정을 생성하는 데 실패했습니다.', error: err.message });
   }
 });
-
 /*---------------------------------------------
-            [로그인]
-            login->signin으로 변경
-            로그인 성공 시 userId와 nickname을 JWT로 담아서 반환합니다.
+    [로그인]
+        로그인 성공 시 userId와 nickname을 JWT로 담아서 반환합니다.
 ---------------------------------------------*/
 router.post('/signin', async (req, res) => {
   const { email, password } = req.body;
-
+  console.log("ㅇㅇ");
   try {
     await signInSchema.validate(req.body);
     const user = await prisma.users.findUnique({
@@ -73,11 +56,10 @@ router.post('/signin', async (req, res) => {
         email,
       },
     });
-
     if (!user) {
+      console.log("사용자가 존재하지 않습니다.");
       return res.status(404).json({ message: '사용자가 존재하지 않습니다.' });
     }
-
     if (await bcrypt.compare(password, user.password)) {
       const token = jwt.sign(
         { userId: user.user_id, nickname: user.nickname },
@@ -89,15 +71,15 @@ router.post('/signin', async (req, res) => {
       return res.status(200).json({ token, userId: user.user_id, nickname: user.nickname });
     }
   } catch (err) {
-    if (err.name === 'ValidationError') {
-      return res.status(400).json({ message: '유효성 검사 오류', error: err.errors });
-    }
     console.log(err);
+    if (err.name === 'ValidationError') {
+      return res.status(400).json({ message: '이메일, 비밀번호를 요구조건에 맞게 입력해주세요', error: err.errors });
+    }
   }
   return res.status(409).json({ message: '비밀번호가 일치하지 않습니다.' });
 });
 /*---------------------------------------------
-            [사용자 정보 조회]
+    [사용자 정보 조회]
 ---------------------------------------------*/
 router.get('/userinfo', async (req, res) => {
   try {
@@ -121,5 +103,4 @@ router.get('/userinfo', async (req, res) => {
     return res.status(404).json({ message: '해당 닉네임의 사용자를 찾을 수 없습니다.' });
   }
 });
-
 export default router;
