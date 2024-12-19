8f970187edeadcdec6f61cccbd5333482a67979e
gudrn4616 <gudrn8293@naver.com>
2024-12-19 10:53:38 +0900

    레디스 수정

--- Server/LobbyServer/src/utils/redis/redis.ts
+++ Server/LobbyServer/src/utils/redis/redis.ts
@@ -1,6 +1,7 @@
 import exp from 'constants';
 import Redis from 'ioredis';
 
-export const redis = new Redis(
-  'redis://ec2-13-125-207-67.ap-northeast-2.compute.amazonaws.com:6379',
-);
+export const redis = new Redis({
+  host: 'ec2-13-125-199-218.ap-northeast-2.compute.amazonaws.com',
+  port: 6379,
+});
