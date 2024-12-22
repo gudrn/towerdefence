61a6c0f09e4e413b621d32780fb6d57724461c4a
조정현 <emforhs0913@naver.com>
2024-12-22 22:03:09 +0900

    주소 변경

--- Server/LobbyServer/src/config/config.ts
+++ Server/LobbyServer/src/config/config.ts
@@ -5,8 +5,7 @@ dotenv.config();
 export const PORT = 3001;
 export const HOST = '0.0.0.0';
 
-export const BATTLE_PORT = 3005;
-export const BATTLE_HOST = 'ec2-13-125-199-218.ap-northeast-2.compute.amazonaws.com';
+export const BATTLE_HOST = 'ec2-43-200-180-165.ap-northeast-2.compute.amazonaws.com';
 
 export const CLIENT_VERSION = process.env.CLIENT_VERSION || '1.0.0';
 
